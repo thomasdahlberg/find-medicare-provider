2ba42705e9862c1e3e79453bea23cd386571cf4f
Thomas Dahlberg <thomasdahlberg@Thomass-MBP.netgear.com>
2019-11-29 07:15:10 -0500

    created development branch, linked betterdoctors api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('js connected!!')
+$.ajax({
+    url: `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=e207638b02ecb23dabc2848f0b3931f6`,
+    type: "GET",
+    // data: {
+      
+    // }
+}).then(function(data) {
+    console.log(data);
+});
